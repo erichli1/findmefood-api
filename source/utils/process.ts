2176b4c4152bb5ae60e26e71f781c43565a78a58
dzheng21 <19882922+dzheng21@users.noreply.github.com>
2022-09-09 23:16:45 -0400

    Implement logic for processing API results
    Creates multiple filter functions to curate results from API. Function
    filterAllResultsByPreferences combines all filter logic to process
    raw results object.

---
+++ source/utils/process.ts
@@ -0,0 +1,130 @@
+/** source/routes/process.ts */
+
+import { ResultFilterParams, PriceOptions } from "./types";
+
+const PLACES_API_OPERATIONAL_STRING = "OPERATIONAL";
+
+/**
+ *
+ * @param results Array of API results from Places API with all restaurant listings
+ * @param params interface with all filter params from user
+ * @returns fully filtered list of results
+ */
+export function fitlerAllResultsByPreferences(
+  results: any[],
+  params: ResultFilterParams
+): any[] {
+  // convert distance and add to the object
+  return results.filter((result) => {
+    return (
+      filterByOpen(result) &&
+      filterRatingsQuality(result, params.minRating, params.minNumReviews) &&
+      filterByDistance(
+        result,
+        params.maxDistance,
+        Number(params.currentLat),
+        Number(params.currentLong)
+      ) &&
+      (params.maxPriceLevel !== undefined
+        ? fitlerByPrice(result, params.maxPriceLevel)
+        : true)
+    );
+  });
+}
+
+/**
+ * Filter util function to calculate whether a result satisfies minRating and minNumReviews constratints.
+ *
+ * @param result — API result from Places API with all restaurants
+ * @param minRating — minimum rating for all eligbile APIs
+ * @returns True if a place's rating is greater than minRating
+ */
+const filterRatingsQuality = (
+  result: any,
+  minRating: number,
+  minNumReviews: number
+): boolean => {
+  return (
+    result.rating >= minRating && result.user_ratings_total >= minNumReviews
+  );
+};
+
+/**
+ * Filter util function to calculate whether a result is within the max distance
+ *
+ * @param result — API result from Places API with all restaurants
+ * @param maxDistance — furthest distance from current location, in miles
+ * @returns True if place element is within the maxDistance
+ */
+const filterByDistance = (
+  result: any,
+  maxDistance: number,
+  currentLat: number,
+  currentLong: number
+): boolean => {
+  const resultLat: number = result.geometry.location.lat;
+  const resultLong: number = result.geometry.location.lng;
+  return (
+    calculateDistanceInMiles(currentLat, currentLong, resultLat, resultLong) <=
+    maxDistance
+  );
+};
+
+/**
+ *
+ * @param result API result from Places API with all restaurants
+ * @returns True if restaurant is currently open and operational
+ */
+const filterByOpen = (result: any): boolean => {
+  return (
+    result.business_status === PLACES_API_OPERATIONAL_STRING &&
+    result.opening_hours.open_now
+  );
+};
+
+/**
+ *
+ * @param result API result from Places API with all restaurants
+ * @param highestPrice int corresponding to a price level (see PriceOptions enum)
+ * @returns True if restaurant/result is within the price range
+ */
+const fitlerByPrice = (result: any, highestPrice: number): boolean => {
+  const highestPriceValue = PriceOptions[highestPrice];
+  return result.price_level <= highestPriceValue;
+};
+
+/**
+ * Util function to calculate the distance between current location and result location in miles
+ * Reference: https://www.geodatasource.com/developers/javascript
+ *
+ * @param currentLat current location of user (latitude)
+ * @param currentLong ''' (longitude)
+ * @param placeLat location of result (latitude)
+ * @param placeLong ''' (longitude)
+ * @returns distance between both locations in miles (as the crow flies)
+ */
+function calculateDistanceInMiles(
+  currentLat: number,
+  currentLong: number,
+  placeLat: number,
+  placeLong: number
+): number {
+  if (currentLat == placeLat && currentLong == placeLong) {
+    return 0;
+  } else {
+    var radlat1 = (Math.PI * currentLat) / 180;
+    var radlat2 = (Math.PI * placeLat) / 180;
+    var theta = currentLong - placeLong;
+    var radtheta = (Math.PI * theta) / 180;
+    var dist =
+      Math.sin(radlat1) * Math.sin(radlat2) +
+      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    if (dist > 1) {
+      dist = 1;
+    }
+    dist = Math.acos(dist);
+    dist = (dist * 180) / Math.PI;
+    dist = dist * 60 * 1.1515;
+    return dist;
+  }
+}

3767c3b8b8f379a37b04ab1223ec019dded08c7d
erichli1 <ehli@college.harvard.edu>
2022-09-10 00:16:47 -0400

    Integrate filtering and processing into restaurant list

--- source/controllers/restaurants.ts
+++ source/controllers/restaurants.ts
@@ -1,25 +1,36 @@
 import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse } from 'axios';
 import url from 'url';
+import { processAllAPIResults } from '../utils/process';
+import { ResultFilterParams } from '../utils/types';
 
 require("dotenv").config();
 
 const getNearbyRestaurants = async (req: Request, res: Response, next: NextFunction) => {
     let frontendParams = url.parse(req.url, true).query;
 
-    const radius = frontendParams.distance as string;
+    const radius = convertStringMilesToMeters(frontendParams.distance as string);
+    const latLongArray = (frontendParams.location as string).split(',');
 
     const params = {
         params: {
             key: process.env.GMAPS_API_KEY,
             location: frontendParams.location,
-            radius: convertStringMilesToMeters(radius),
-            type: 'restaurant'
+            radius: radius,
+            type: 'restaurant',
+            opennow: true
         }
     }
 
+    const resultFilters: ResultFilterParams = {
+        minRating: parseFloat(frontendParams.stars as string),
+        minNumReviews: parseFloat(frontendParams.reviews as string),
+        currentLat: latLongArray[0],
+        currentLong: latLongArray[1],
+    }
+
     let result: AxiosResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json`, params );
-    return res.status(200).json(result.data)
+    return res.status(200).json(processAllAPIResults(result.data.results, resultFilters))
 }
 
 function convertStringMilesToMeters(distanceInMiles: string) {
